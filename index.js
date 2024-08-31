f269f6a3ae14f05207e6e7dfbb3c608a96813978
Babiel09 <estudosbabiel@gmail.com>
2024-08-30 21:34:09 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,61 @@
 class Cliente{
-    constructor(nome, cpf, agencia, saldo){
+    constructor(nome, cpf){
     this.nome = nome,
-    this.cpf = cpf,
-    this.agencia = agencia,
-    this. saldo = saldo
+    this.cpf = cpf
     }
 }
+
+//Criando a conta corrente:
+
+class ContaCorrente{
+    constructor(agencia, saldo){
+    this.agencia = agencia;
+    this.saldo = saldo;
+    }
+
+//Criando metodos:
+
+saque(valor){
+    if(this.saldo >= valor){
+        this.saldo -= valor
+        }
+    }  
+}
+
+
+
 
 const cliente1 = new Cliente()
 
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
+
+//Add ao cliente1 a conta corrente:
+
+const cliente1conta = new ContaCorrente()
+cliente1conta.agencia = 1001;
+cliente1conta.saldo = 1000;
 
 const cliente2 = new Cliente()
 
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.group(cliente1)
-console.group(cliente2)
+//Fazendo a mesma coisa com o cliente2:
+
+const cliente2conta = new ContaCorrente()
+
+cliente2conta.agencia = 1001;
+cliente2conta.saldo = 1000;
+
+cliente1conta.saque(400)
+cliente2conta.saque(4000)
+
+
+
+
+
+
+
+console.log(cliente1conta.saldo)
+console.log(cliente2conta.saldo)
